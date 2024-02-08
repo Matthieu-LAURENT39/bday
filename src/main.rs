b411f9f482c7738fc4caeebb6594ed6e93c821f3
Matthieu LAURENT <matthieu.laurent69@proton.me>
2024-02-08 09:55:23 +0100

    Load config files from proper places

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use chrono::{DateTime, Datelike, Duration, Local, NaiveDate, NaiveTime, TimeZone};
 use chrono_humanize::HumanTime;
 use chrono_tz::Tz;
-use clap::{error::ErrorKind, Command, CommandFactory, Parser, Subcommand};
-use core::time;
+use clap::{error::Error, error::ErrorKind, Command, CommandFactory, Parser, Subcommand};
+use directories::BaseDirs;
 use prettytable::{format, row, Table};
 use serde::{Deserialize, Serialize};
+use std::path::{self, Path};
 use std::{fs, process::exit};
-use toml::value::Date;
+
+const CONFIG_FILE_NAME: &str = "rust-birthday.toml";
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
@@ -49,6 +51,77 @@ struct Config {
     birthdays: Vec<Entry>,
 }
 
+impl Default for Config {
+    fn default() -> Self {
+        Self { birthdays: vec![] }
+    }
+}
+
+struct ConfigFile {
+    path: path::PathBuf,
+    config: Config,
+}
+
+impl Default for ConfigFile {
+    fn default() -> Self {
+        let path = BaseDirs::new()
+            .map(|dirs| dirs.config_dir().join(CONFIG_FILE_NAME))
+            // Fallback to a hardcoded path if BaseDirs::new() returns None
+            .unwrap_or_else(|| path::PathBuf::from("~/.config/").join(CONFIG_FILE_NAME));
+
+        Self {
+            // Default to $XDG_CONFIG_HOME/birthdays.toml
+            path,
+            config: Config::default(),
+        }
+    }
+}
+
+enum LoadConfigError {
+    /// A config file was found, but there was an error reading it
+    IoError(std::io::Error),
+    /// A config file was found, but there was an error parsing it
+    TomlError(toml::de::Error),
+    /// No valid config file found
+    ConfigNotFound,
+}
+
+/// Load the config file  
+/// The priority is in that order:
+/// - ./birthdays.toml
+/// - $XDG_CONFIG_HOME/birthdays.toml
+/// - $HOME/.config/birthdays.toml
+/// - $HOME/.birthdays.toml
+fn load_config() -> Result<ConfigFile, LoadConfigError> {
+    for path in [
+        //? ./birthdays.toml
+        Path::new(".").join(CONFIG_FILE_NAME),
+        //? $XDG_CONFIG_HOME/birthdays.toml
+        BaseDirs::new()
+            // TODO: Remove this unwrap
+            .unwrap()
+            .config_dir()
+            .join(CONFIG_FILE_NAME),
+        //? $HOME/.config/birthdays.toml
+        Path::new("~/.config/").join(CONFIG_FILE_NAME),
+        //? $HOME/.birthdays.toml
+        Path::new("~/").join(".".to_owned() + CONFIG_FILE_NAME),
+    ]
+    .iter()
+    {
+        if path.is_file() {
+            let toml_str = fs::read_to_string(path).map_err(LoadConfigError::IoError)?;
+            return toml::from_str(&toml_str)
+                .map_err(LoadConfigError::TomlError)
+                .map(|config| ConfigFile {
+                    path: path.to_path_buf(),
+                    config,
+                });
+        }
+    }
+    Err(LoadConfigError::ConfigNotFound)
+}
+
 /// Get the next occurence of a date
 fn get_next_occurence(date: NaiveDate, timezone: Option<Tz>) -> DateTime<Local> {
     let today = Local::now();
@@ -86,25 +159,39 @@ fn get_next_occurence(date: NaiveDate, timezone: Option<Tz>) -> DateTime<Local> 
     }
 }
 
+/// Exit codes:  
+/// 0: Success. Note that this is still returned if no entries are found, but
+///    the program will print an error message to stderr in that case, leaving stdout empty.  
+/// 2: Invalid command, or other clap parsing error  
+/// 3: Error reading or parsing the config file  
 fn main() {
     let cli = Cli::parse();
 
-    let mut toml_entries: Config;
-    if let Ok(toml_str) = fs::read_to_string("./test.toml") {
-        match toml::from_str(&toml_str) {
-            Ok(parsed) => toml_entries = parsed,
-            Err(e) => Cli::command()
-                .error(
-                    ErrorKind::Io,
-                    format!("Error parsing toml file:\n{}\nYou can delete the file, it will be recreated the next time you add a new birthday.", e),
-                )
-                // TODO: change the error code to 3
-                // TODO: remove the "usage: " section that gets displayed
-                .exit(),
-        }
-    } else {
-        toml_entries = Config { birthdays: vec![] };
-    }
+    let mut conf_file: ConfigFile = match load_config() {
+        Ok(cfg) => cfg,
+        Err(e) => match e {
+            // Use a default config if no config file is found
+            LoadConfigError::ConfigNotFound => ConfigFile::default(),
+            // TODO: Use clap to display the error message
+            LoadConfigError::IoError(e) => {
+                eprintln!("Error reading config file: {}", e);
+                exit(1);
+            }
+            LoadConfigError::TomlError(e) => {
+                eprintln!("Error parsing config file: {}", e);
+                exit(1);
+            }
+        },
+    };
+
+    // Err(e) => Cli::command()
+    //     .error(
+    //         ErrorKind::Io,
+    //         format!("Error parsing toml file:\n{}\nYou can delete the file, it will be recreated the next time you add a new birthday.", e),
+    //     )
+    //     // TODO: change the error code to 3
+    //     // TODO: remove the "usage: " section that gets displayed
+    //     .exit(),
 
     match &cli.command {
         Commands::Add {
@@ -130,14 +217,14 @@ fn main() {
                     None => None,
                 },
             };
-            toml_entries.birthdays.push(new_entry);
-            let toml_str = toml::to_string(&toml_entries).expect("Error serializing toml");
-            fs::write("./test.toml", toml_str).expect("Error writing toml file");
+            conf_file.config.birthdays.push(new_entry);
+            let toml_str = toml::to_string(&conf_file.config).expect("Error serializing toml");
+            fs::write(conf_file.path, toml_str).expect("Error writing toml file");
         }
         Commands::List => {
-            if toml_entries.birthdays.is_empty() {
+            if conf_file.config.birthdays.is_empty() {
                 eprintln!("No entries found, add some with the 'add' command.");
-                exit(1);
+                exit(0);
             }
             // List all entries
             // for entry in toml_entries.birthdays.iter() {
@@ -156,7 +243,7 @@ fn main() {
             table.set_format(*format::consts::FORMAT_BOX_CHARS);
 
             table.add_row(row!["Name", "Date", "Age", "In"]);
-            for entry in toml_entries.birthdays.iter() {
+            for entry in conf_file.config.birthdays.iter() {
                 let timezone: Option<Tz>;
                 if let Some(tz) = &entry.timezone {
                     match Tz::from_str_insensitive(&tz) {
